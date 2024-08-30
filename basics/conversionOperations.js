61e7728d8f81c25911e2c6ea69cc8a6bfdf93081
Arpan <arpan794@gmail.com>
2024-08-30 13:03:28 +0530

    Operations

--- basics/conversionOperations.js
+++ basics/conversionOperations.js
@@ -19,3 +19,31 @@ console.log(booleanIsLoggedIn);
 // 1=> true; 0=> false
 // "" => false
 // "Arpan" => true
+
+// ********************************* Operations *****************************
+
+let value = 3
+let negValue= -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = "Arpan"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+console.log(+true)
+console.log(+"")
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
