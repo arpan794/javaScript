a475ffc1412a6a396501e1165ed3bf145b4e607a
Arpan <arpan794@gmail.com>
2024-09-21 11:43:56 +0530

    iteration files

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -4,7 +4,10 @@ const myNumbers = [1,2,3,4,5,6,7,8,9]
 //     return num + 10;
 // })
 
+const newNums = myNumbers
+               .map( (num) => num * 10)
+               .map( (num) => num + 1)
+               .filter( (num) => num >=40)
 
-
-console.log(newNum);
+console.log(newNums);
 
